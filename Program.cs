10d57d3803cf56a480fea2ce45608f83fdffadcf
Juan Alberto Hernandez <jualherac@gmail.com>
2022-06-08 21:08:43 -0500

    Initial Commit, add the dictionay and the transaction function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Dictionary<int,int> vendingMachine = new Dictionary<int,int>();
+vendingMachine.Add(1,20);
+vendingMachine.Add(2, 20);
+vendingMachine.Add(5,20);
+vendingMachine.Add(10,20);
+vendingMachine.Add(20,20);
+
+int itemPrice = 0;
+int amountOfPayment = 0;
+
+
+
+int Transaction(Dictionary<int,int>vendingMachine, int itemPrice, int amountOfPayment)
+{
+    int amountOfReturn = itemPrice - amountOfPayment;
+
+
+    return amountOfReturn;
+}
+

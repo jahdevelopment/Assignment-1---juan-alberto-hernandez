ca10bc588fc3fd2d27cc0a49eb78d27164cd3a12
Juan Alberto Hernandez <jualherac@gmail.com>
2022-06-10 23:15:15 -0500

    add a foreach loop for iterate between the coins of the vending machine

--- Program.cs
+++ Program.cs
@@ -1,21 +1,46 @@
-﻿
-Dictionary<int,int> vendingMachine = new Dictionary<int,int>();
-vendingMachine.Add(1,20);
-vendingMachine.Add(2, 20);
-vendingMachine.Add(5,20);
-vendingMachine.Add(10,20);
-vendingMachine.Add(20,20);
+﻿Console.WriteLine("Please enter the price of the product:");
+int itemPrice = Int32.Parse(Console.ReadLine());
 
-int itemPrice = 0;
-int amountOfPayment = 0;
+Console.WriteLine("Please enter the amount of payment:");
+int amountOfPayment = Int32.Parse(Console.ReadLine());
 
+Transaction(itemPrice, amountOfPayment);
 
-
-int Transaction(Dictionary<int,int>vendingMachine, int itemPrice, int amountOfPayment)
+int Transaction( int itemPrice, int amountOfPayment)
 {
-    int amountOfReturn = itemPrice - amountOfPayment;
+    Dictionary<int, int> vendingMachine = new Dictionary<int, int>();
+    vendingMachine.Add(20, 24);
+    vendingMachine.Add(10, 23);
+    vendingMachine.Add(5, 22);
+    vendingMachine.Add(2, 21);
+    vendingMachine.Add(1, 20);
 
+    int amountOfReturn = amountOfPayment - itemPrice;
+    Console.WriteLine(amountOfReturn);
 
+    while (amountOfReturn > 0 && itemPrice != amountOfPayment)
+    {
+        foreach (KeyValuePair<int, int> item in vendingMachine)
+
+            if (item.Key.Equals(amountOfReturn))
+            {
+                Console.WriteLine($"the amount of return is: {amountOfReturn}");
+                //item.Value-=1;
+                Console.WriteLine(item.Value);
+                amountOfReturn = 0;
+                
+            }
+            //else if (item.key < amountOfPayment)
+            //{
+
+            //}
+    }
     return amountOfReturn;
 }
+
+
+    
+
+
+
 
